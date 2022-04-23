a84c4de4414f17362b9974dc4947ed042ce52356
iigmir <roc120j@gmail.com>
2022-04-23 19:47:48 +0800

    Add the table component

--- src/views/TableView/TheTable.vue
+++ src/views/TableView/TheTable.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="the-table">
-        <!--  -->
+        <p>THe Table</p>
     </div>
 </template>
 
--- src/views/TableView/index.vue
+++ src/views/TableView/index.vue
@@ -1,12 +1,18 @@
 <template>
     <b-container class="table">
         <h1>{{ title }}</h1>
+        <the-table />
     </b-container>
 </template>
 
 <script>
+import TheTable from "./TheTable.vue";
+
 export default {
     name: "TableView",
+    components: {
+        TheTable
+    },
     data: () => ({
         title: "Hello"
     })
