0884efcd5c9e740d1e1c0fad70e63427bb695679
iigmir <roc120j@gmail.com>
2022-04-23 19:50:25 +0800

    Basic table

--- src/views/TableView/TheTable.vue
+++ src/views/TableView/TheTable.vue
@@ -1,11 +1,27 @@
 <template>
     <div class="the-table">
-        <p>THe Table</p>
+        <p>Below tbale</p>
+        <b-table striped hover v-bind="{ items, fields }">
+            <!--  -->
+        </b-table>
     </div>
 </template>
 
 <script>
 export default {
     name: "TheTable",
+    data: () => ({
+        items: [
+          { isActive: true, age: 40, first_name: "Dickerson", last_name: "Macdonald" },
+          { isActive: false, age: 21, first_name: "Larsen", last_name: "Shaw" },
+          { isActive: false, age: 89, first_name: "Geneva", last_name: "Wilson" },
+          { isActive: true, age: 38, first_name: "Jami", last_name: "Carney" }
+        ]
+    }),
+    computed: {
+        fields() {
+            return [];
+        },
+    }
 }
 </script>
