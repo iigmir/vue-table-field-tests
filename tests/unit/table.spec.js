26dc099c367fe6e986b3332d9043028d44ffe8f4
iigmir <roc120j@gmail.com>
2022-04-23 20:00:04 +0800

    Add BTable

--- tests/unit/table.spec.js
+++ tests/unit/table.spec.js
@@ -1,10 +1,13 @@
 import { strictEqual } from "assert";
-import { shallowMount } from "@vue/test-utils"
+import { shallowMount } from "@vue/test-utils";
 import TheTable from "@/views/TableView/TheTable.vue";
+import { BTable } from "bootstrap-vue";
 
 describe("TheTable.vue", () => {
     it("renders props.msg when passed", () => {
-        const wrapper = shallowMount(TheTable);
+        const wrapper = shallowMount(TheTable, {
+            components: { BTable }
+        });
         const fields = wrapper.vm.fields;
         strictEqual( fields.length, 3 );
     })
