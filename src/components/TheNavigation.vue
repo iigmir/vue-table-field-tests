fb68fea5f01af556908d1cca675a2aa31b2121d0
iigmir <roc120j@gmail.com>
2022-04-23 19:30:44 +0800

    Add ne link

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -18,6 +18,7 @@ export default {
         links: [
             { to: "/", name: "Home" },
             { to: "/about", name: "About" },
+            { to: "/table", name: "Table" },
         ]
     })
 };
