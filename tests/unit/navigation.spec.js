d30bbb2b9fa323f3b5c451974e00a25397fd35f0
iigmir <roc120j@gmail.com>
2022-04-23 19:38:44 +0800

    Lintings part 2

--- tests/unit/navigation.spec.js
+++ tests/unit/navigation.spec.js
@@ -5,12 +5,12 @@ import { strictEqual } from "assert";
 describe( "TheNavigation.vue", () => {
     it( "has unique links", () => {
         const wrapper = shallowMount( TheNavigation, {
-            stubs: ['router-link']
+            stubs: ["router-link"]
         });
         const links = wrapper.vm.links.map( ({ to }) => to );
-        const result = links.filter( (item, index, array) => {
-            return array.indexOf(item) !== array.lastIndexOf(item)
-        });
+        const result = links.filter( (item, index, array) =>
+            array.indexOf(item) !== array.lastIndexOf(item)
+        );
         strictEqual( result.length, 0 );
     })
 })
