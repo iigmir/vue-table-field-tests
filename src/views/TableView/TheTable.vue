9b7f0131da7b9117ae77cae743510ea73425f763
iigmir <roc120j@gmail.com>
2022-04-23 19:51:56 +0800

    lintings

--- src/views/TableView/TheTable.vue
+++ src/views/TableView/TheTable.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="the-table">
-        <p>Below tbale</p>
-        <b-table striped hover v-bind="{ items, fields }">
-            <!--  -->
-        </b-table>
-    </div>
+  <div class="the-table">
+    <p>Below tbale</p>
+    <b-table striped hover v-bind="{ items, fields }">
+      <!--  -->
+    </b-table>
+  </div>
 </template>
 
 <script>
@@ -12,16 +12,36 @@ export default {
     name: "TheTable",
     data: () => ({
         items: [
-          { isActive: true, age: 40, first_name: "Dickerson", last_name: "Macdonald" },
-          { isActive: false, age: 21, first_name: "Larsen", last_name: "Shaw" },
-          { isActive: false, age: 89, first_name: "Geneva", last_name: "Wilson" },
-          { isActive: true, age: 38, first_name: "Jami", last_name: "Carney" }
-        ]
+            {
+                isActive: true,
+                age: 40,
+                first_name: "Dickerson",
+                last_name: "Macdonald",
+            },
+            {
+                isActive: false,
+                age: 21,
+                first_name: "Larsen",
+                last_name: "Shaw",
+            },
+            {
+                isActive: false,
+                age: 89,
+                first_name: "Geneva",
+                last_name: "Wilson",
+            },
+            {
+                isActive: true,
+                age: 38,
+                first_name: "Jami",
+                last_name: "Carney",
+            },
+        ],
     }),
     computed: {
         fields() {
             return [];
         },
-    }
-}
+    },
+};
 </script>
