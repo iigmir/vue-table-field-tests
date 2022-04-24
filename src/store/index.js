9953a7dfa1618a7145dd6758a349d0cc8488be07
iigmir <roc120j@gmail.com>
2022-04-24 20:02:40 +0800

    Add vuex modules

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,11 @@
 import Vue from "vue"
 import Vuex from "vuex"
+import listid from "./modules/listid.js";
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    modules: {
+        listid
+    }
 })
