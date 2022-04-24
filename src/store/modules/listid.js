b8bce8ba1aa2c043d4a93f9ba9f41a25b609364e
iigmir <roc120j@gmail.com>
2022-04-24 20:14:14 +0800

    Add store

--- src/store/modules/listid.js
+++ src/store/modules/listid.js
@@ -1,10 +1,24 @@
+import ListData from "./example.json";
+
+const AJAX = Promise.resolve( ListData );
+
 export default {
+    namespaced: true,
     state: {
+        list: []
     },
     getters: {
     },
     mutations: {
+        SET_list(state, list = []) {
+            state.list = list;
+        }
     },
     actions: {
+        GET_list({ commit }) {
+            AJAX.then(
+                (ajaxlist) => commit("SET_list", ajaxlist)
+            );
+        }
     },
 };
